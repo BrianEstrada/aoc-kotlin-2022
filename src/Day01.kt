382b9f07157d39bcd64b757f2b4c924c3325c226
Brian Estrada <lovesguitar@gmail.com>
2022-12-01 23:31:16 +0100

    day 1 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
+
+    fun getElves(input: List<String>): IntArray {
+        val elfs = IntArray(input.count { it.isEmpty() } + 1) { 0 }
+        var elfIndex = 0
+
+        for (value in input) {
+            if (value.isEmpty()) {
+                elfIndex++
+            } else {
+                elfs[elfIndex] += value.toInt()
+            }
+        }
+
+        return elfs
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getElves(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getElves(input).sortedArrayDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("Part 1: " + part1(readInput("Day01_test")))
+    println("Part 1: " + part1(readInput("Day01")))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 2: " + part2(readInput("Day01_test")))
+    println("Part 2: " + part2(readInput("Day01")))
 }
